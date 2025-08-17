29d251c9f5dc36ce7b2f2c2a6fd5872beb0cc6c1
xoanross <xoanross@gmail.com>
2025-08-17 21:06:52 +0200

    Initial commit

---
+++ src/test/java/org/xoanross/starwars/backend/StarwarsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xoanross.starwars.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
